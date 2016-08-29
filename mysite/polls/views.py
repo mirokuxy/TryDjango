0ef82fb458325643a274442534cb2ca2acab4f0d
mirokuxy <mirokuxy@gmail.com>
2016-08-28 19:13:53 -0700

    implements voting;

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
 
-from polls.models import Question 
+from polls.models import Question, Choice
 
 
 # Create your views here.
@@ -19,10 +20,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
-    response = "you're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', { 'question':question})
 
 def vote(request, question_id):
-    response = "You're voting on question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+                      'question': question,
+                      'error_message': "You didn't select a choice.",
+                      })
+
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+
+
 
