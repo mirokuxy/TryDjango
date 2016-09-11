265185d26a1ed92ed4cc2bb5314794a219bde281
mirokuxy <mirokuxy@gmail.com>
2016-09-10 23:40:50 -0700

    customize admin page look;

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -8,6 +8,10 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
     #fields = ['pub_date', 'question_text']
     fieldsets = [
         (None,  {'fields': ['question_text']} ),
